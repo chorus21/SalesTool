df7a7e1ebfadf8348887a14922e115e4e2738a74
ykang <ykang@kia.com.au>
2018-07-26 22:43:45 +1000

    change default values and refactor for loop.

--- SalesTool/src/ca/demo/salestool/SalesData.java
+++ SalesTool/src/ca/demo/salestool/SalesData.java
@@ -1,13 +1,18 @@
 package ca.demo.salestool;
 
 public class SalesData {
-	int data[] = {0, 4, 42};
+	int data[] = {-1, 0, 4, 42, 84};
 	
 	public void display() {
 		System.out.println("Data: ");
-		for(int i = 0; i < data.length; i++) {
+//      replaced by foreach loop
+		for(int i: data) {
+			System.out.println("Next value: " + i);
+		}
+/*
+ 		for(int i = 0; i < data.length; i++) {
 			System.out.println("Next value: " + data[i]);
 		}
-
+*/
 	}
 }
